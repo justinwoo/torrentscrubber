f4f7b3812edbb7f70390685ffd0f02d64df6023e
Justin Woo <moomoowoo@gmail.com>
2014-02-01 01:12:53 -0600

    changes and stuff

--- main.rb
+++ main.rb
@@ -13,9 +13,29 @@ shows = config["shows"]
 driver = Selenium::WebDriver.for(:firefox)
 driver.navigate.to(url)
 elements = driver.find_elements(:css, 'tr.trusted.tlistrow > td.tlistname > a')
+links = elements.map {
+  |element|
+  href = element.attribute("href")
+  title = element.text
+  [href, title]
+}
 
-elements.map { |x| puts x }
+# links.each_with_index {
+#   |x, i|
+#   puts "#{i}: #{x}"
+# }
+
+# IO.write("output.txt", JSON.pretty_generate(links))
+
+# links.map { 
+#   |link|
+#   puts "link: #{link}"
+#   driver.navigate.to(link)
+#   date = driver.find_element(:css, '.vtop').text
+#   puts "date #{date}"
+# }
 
 
 # output = JSON.pretty_generate(config)
 # IO.write("config.json", output)
+  
